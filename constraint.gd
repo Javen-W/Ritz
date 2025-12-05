8a17d3fc46729e88048e45fe1fed3899fde22764
Javen William Zamojcin <javen.william@gmail.com>
2025-12-05 12:49:44 -0500

    impl neq constraint

--- constraint.gd
+++ constraint.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name Constraint
 
-enum Type { SUM, EQUAL, LESS_THAN, GREATER_THAN }
+enum Type { SUM, EQUAL, LESS_THAN, GREATER_THAN, NOT_EQUAL }
 
 @export var type : Type = Type.SUM
 @export var group : Array[Tile] = []
@@ -39,6 +39,8 @@ func _ready() -> void:
 		label.text = "" + str(target_value)
 	elif type == Type.EQUAL:
 		label.text = "="
+	elif type == Type.NOT_EQUAL:
+		label.text = "!="
 	elif type == Type.LESS_THAN:
 		label.text = "<" + str(target_value)
 	elif type == Type.GREATER_THAN:
@@ -52,21 +54,19 @@ func generate(rng: RandomNumberGenerator) -> void:
 		
 	# Group metrics
 	var group_sum = 0
-	var group_min = INF
-	var group_max = -INF
 	for t in group:
 		print("Constraint Tile: position={0}, value={1}".format([t.position / 64.0, t.value]))
 		group_sum += t.value
-		group_min = min(group_min, t.value)
-		group_max = max(group_max, t.value)
 	
 	# Determine constraint type
-	if group.size() > 1 and group_min == group_max and rng.randf() < 0.85:
+	if group.size() > 1 and is_equal() and rng.randf() > 0.15:
 		type = Constraint.Type.EQUAL
-	elif rng.randf() < 0.10:
+	elif group.size() > 1 and is_notequal() and rng.randf() < 0.15:
+		type = Constraint.Type.NOT_EQUAL
+	elif group_sum <= 10 and rng.randf() < 0.10:
 		type = Constraint.Type.LESS_THAN
-		target_value = rng.randi_range(group_sum + 1, group_sum + 7)
-	elif rng.randf() < 0.10:
+		target_value = rng.randi_range(group_sum + 1, group_sum + 6)
+	elif group_sum > 0 and rng.randf() < 0.10:
 		type = Constraint.Type.GREATER_THAN
 		target_value = rng.randi_range(0, group_sum - 1)
 	else:
@@ -75,4 +75,18 @@ func generate(rng: RandomNumberGenerator) -> void:
 	
 	# Determine color
 	color = Color.from_hsv(rng.randf(), 0.95, 1.0, 0.35)
-	
+
+func is_equal() -> bool:
+	var vals = []
+	for t in group:
+		vals.append(t.value)
+	return vals.min() == vals.max()
+
+func is_notequal() -> bool:
+	var t_group = group.duplicate()
+	var first_val = t_group.pop_front().value
+	for t in t_group:
+		if t.value == first_val:
+			return false
+	return true
+		
