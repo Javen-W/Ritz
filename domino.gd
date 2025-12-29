f220d10535962bbebb3141d3958bc2c0ed44c97a
Javen William Zamojcin <javen.william@gmail.com>
2025-12-29 14:49:49 -0500

    impl domino-tile assignment

--- domino.gd
+++ domino.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name Domino
 
 signal domino_released
+signal domino_picked
 
 @export var tile1 : Tile = null
 @export var tile2 : Tile = null
@@ -12,6 +13,8 @@ signal domino_released
 
 @onready var background : MeshInstance2D = $Background
 @onready var shader_material : ShaderMaterial = background.material as ShaderMaterial
+@onready var dots1_collision : CollisionShape2D = $Dots1Area2D/CollisionShape2D
+@onready var dots2_collision : CollisionShape2D = $Dots2Area2D/CollisionShape2D
 
 func _ready() -> void:
 	update_pips()
@@ -24,11 +27,13 @@ func init(dots1_val: int, dots2_val: int, horizontal: bool = true) -> void:
 	self.is_horizontal = horizontal
 	print("Domino init(): dots1_value{0}, dots2_value{1}".format([dots1_val, dots2_val]))
 
-func update_position_to_tiles(t1: Tile, t2: Tile, offset: Vector2) -> void:
+func update_position_to_tiles(t1: Tile, t2: Tile, offset: Vector2) -> bool:
 	if t1 != null and t2 != null and t1 != t2:
 		print(t1.position.distance_to(t2.position))
 		var center = (t1.position + t2.position) * 0.5
 		self.position = center + offset
+		return true
+	return false
 
 func update_pips() -> void:
 	if shader_material:
@@ -54,23 +59,24 @@ func _on_mouse_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx:
 			rotate_once()
 		elif event.is_pressed():
 			is_pressed = true
-			print("Mouse pressed.")
+			domino_picked.emit()
+			# print("Mouse pressed.")
 		elif event.is_released() and is_pressed:
 			is_pressed = false
 			domino_released.emit()
-			print("Mouse released.")
+			# print("Mouse released.")
 
 func _on_dots_1_area_2d_area_entered(area: Area2D) -> void:
 	var tile = area.owner as Tile
 	if !entered_tile1s.has(tile):
 		entered_tile1s.append(tile)
-		print("Domino dots1 area entered: ", tile)
+		# print("Domino dots1 area entered: ", tile)
 		
 func _on_dots_2_area_2d_area_entered(area: Area2D) -> void:
 	var tile = area.owner as Tile
 	if !entered_tile2s.has(tile):
 		entered_tile2s.append(tile)
-		print("Domino dots2 area entered: ", tile)
+		# print("Domino dots2 area entered: ", tile)
 
 func _on_dots_1_area_2d_area_exited(area: Area2D) -> void:
 	var tile = area.owner as Tile
@@ -81,16 +87,12 @@ func _on_dots_2_area_2d_area_exited(area: Area2D) -> void:
 	call_deferred("_erase_entered_tile2", tile)
 
 func _erase_entered_tile2(tile: Tile) -> void:
-	entered_tile2s.erase(tile)
-	if tile == self.tile2:
-		self.tile2 = null
-	print("Domino dots2 area exited: ", tile)
+	entered_tile2s.erase(tile)		
+	# print("Domino dots2 area exited: ", tile)
 
 func _erase_entered_tile1(tile: Tile) -> void:
-	entered_tile1s.erase(tile)
-	if tile == self.tile1:
-		self.tile1 = null
-	print("Domino dots1 area exited: ", tile)
+	entered_tile1s.erase(tile)		
+	# print("Domino dots1 area exited: ", tile)
 
 func _find_nearest_tile(tiles: Array[Tile], dots_pos: Vector2) -> Tile:
 	var min_dist = INF
@@ -102,12 +104,45 @@ func _find_nearest_tile(tiles: Array[Tile], dots_pos: Vector2) -> Tile:
 			min_tile = t
 	return min_tile
 
-func _on_domino_released() -> void:
-	self.tile1 = _find_nearest_tile(entered_tile1s, $Dots1Area2D/CollisionShape2D.global_position)
+func _on_domino_picked() -> void:
 	if self.tile1 != null:
-		entered_tile2s.erase(self.tile1)
-		self.tile2 = _find_nearest_tile(entered_tile2s, self.tile1.position)
+		self.tile1.remove_dots()
+		self.tile1 = null
 	
-	if self.tile1 != null and self.tile2 != null and self.tile1.position.distance_to(self.tile2.position) == 64.0:
-		update_position_to_tiles(self.tile1, self.tile2, Vector2i.ZERO)
-	print(self.tile1, self.tile2)
+	if self.tile2 != null:
+		self.tile2.remove_dots()
+		self.tile2 = null
+
+func _on_domino_released() -> void:
+	# Select & validate candidate tile1.
+	var candidate_tile1 = _find_nearest_tile(entered_tile1s, dots1_collision.global_position)
+	if candidate_tile1 == null:
+		return
+	
+	# Select & validate candidate tile2.
+	var temp_tile2s = entered_tile2s.duplicate()
+	temp_tile2s.erase(candidate_tile1)
+	var candidate_tile2 = _find_nearest_tile(temp_tile2s, candidate_tile1.position)
+	if candidate_tile2 == null:
+		return
+	
+	# Validate candidate tile distance.
+	var tile_dist = candidate_tile1.position.distance_to(candidate_tile2.position)
+	if tile_dist != 64.0:
+		return
+	
+	# Attempt domino position update.
+	var pos_updated = update_position_to_tiles(candidate_tile1, candidate_tile2, Vector2i.ZERO)
+	if !pos_updated:
+		return
+	
+	# Successful candidate tile selections.
+	self.tile1 = candidate_tile1
+	self.tile1.place_dots(self.dots1_value)
+	
+	self.tile2 = candidate_tile2
+	self.tile2.place_dots(self.dots2_value)
+	
+	# Debug.
+	print(self.tile1.global_position, " ", self.tile2.global_position)
+	print(dots1_collision.global_position, " ", dots2_collision.global_position)
--- tile.gd
+++ tile.gd
@@ -9,6 +9,8 @@ signal dots_placed
 func place_dots(v: int) -> void:
 	self.dots_value = v
 	dots_placed.emit()
+	print("Tile place_dots(): ", self.dots_value, " ", self.name)
 
 func remove_dots() -> void:
 	place_dots(-1)
+	# print("Tile remove_dots(): ", self.name)
