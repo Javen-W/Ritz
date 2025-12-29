823ae4fc467f4431682f324c0a8979f26055dca7
Javen William Zamojcin <javen.william@gmail.com>
2025-12-29 14:06:35 -0500

    impl tile dots placing

--- tile.gd
+++ tile.gd
@@ -1,5 +1,14 @@
 extends Node2D
 class_name Tile
 
+signal dots_placed
+
 @export var generated_value : int = -1
-@export var assigned_value : int = -1
+@export var dots_value : int = -1
+
+func place_dots(v: int) -> void:
+	self.dots_value = v
+	dots_placed.emit()
+
+func remove_dots() -> void:
+	place_dots(-1)
